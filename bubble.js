91490ca66094165d4ca5a1fc0ace51e0994d44c8
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-24 10:01:43 -0400

    write bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+	let swapped = false;
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i; j++) {
+			let temp = arr[j];
+			if (arr[j] > arr[j + 1]) {
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				swapped = true;
+			}
+		}
+		if (swapped === false) break;
+	}
+	return arr;
+}
 
 module.exports = bubbleSort;
