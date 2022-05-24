ab0b272b47202b6ab66c8e8a64874845082bd605
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-24 10:06:54 -0400

    write merge

--- merge.js
+++ merge.js
@@ -1,5 +1,29 @@
-function merge() {}
+function merge(arr1, arr2) {
+	let i = 0;
+	let j = 0;
+	const merged = [];
+
+	while (i < arr1.length && j < arr2.length) {
+		if (arr1[i] <= arr2[j]) {
+			merged.push(arr1[i]);
+			i++;
+		}
+		if (arr2[j] <= arr1[i]) {
+			merged.push(arr2[j]);
+			j++;
+		}
+	}
+	while (i < arr1.length) {
+		merged.push(arr1[i]);
+		i++;
+	}
+	while (j < arr2.length) {
+		merged.push(arr2[j]);
+		j++;
+	}
+	return merged;
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
